e804a89a0594546b486f71574ba57f83030fa582
mak <pablo_57@mail.ru>
2023-02-15 00:38:16 +0300

    Simple storage added (server)

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -4,19 +4,54 @@ import (
 	"fmt"
 )
 
+
+
+func (g GMetric) String() string {
+	x := fmt.Sprintf("%v", float64(g.Val))
+	return x
+}
+func (c CMetric) String() string {
+	x := fmt.Sprintf("%v", int64(c.Val))
+	return x
+}
+
 type gauge float64
 type counter int64
 
-func (g gauge) String() string {
-	x := fmt.Sprintf("%v", float64(g))
-	return x
-}
-func (c counter) String() string {
-	x := fmt.Sprintf("%v", int64(c))
-	return x
+type GMetric struct 
+{
+	Name string
+	Val gauge 
 }
 
-type metricset struct {
-	gauges    map[string]gauge
-	PollCount counter
+type CMetric struct
+{
+	Name string
+	Val counter
 }
+
+type MemStorage struct {
+	Gauges    map[string]gauge
+	Counters  map[string]counter
+}
+
+
+type Repository interface{
+	Init()
+	AddGauge(g GMetric)
+	AddCounter(c CMetric)
+} 
+
+func (m *MemStorage) Init (){
+    m.Gauges=make(map[string]gauge)
+	m.Counters=make(map[string]counter)
+}
+
+func (m *MemStorage) AddGauge(g GMetric){
+    m.Gauges[g.Name]=g.Val
+}
+
+func (m *MemStorage) AddCounter(c CMetric){
+    m.Counters[c.Name]=c.Val
+}
+
