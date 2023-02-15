56c76924981f4488446ef07f2fc40f388160c98b
mak <pablo_57@mail.ru>
2023-02-16 00:56:55 +0300

    Created server with storage and handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/rebus2015/praktikum-devops/tree/main/internal/storage"
+//import "../../internal/storage"
+import (
+	"log"
+	"net/http"
+
+	"github.com/rebus2015/praktikum-devops/internal/handlers"
+	"github.com/rebus2015/praktikum-devops/internal/storage"
+)
 
 func main() {
 	s := storage.MemStorage{}
 	s.Init()
-
+	handlers.MemStats = s
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", handlers.ErrorHandleFunc)
+	mux.HandleFunc("/update/counter", handlers.UpdateCounterHandlerFunc)
+	mux.HandleFunc("/update/gauge", handlers.UpdateGaugeHandlerFunc)
+	// конструируем свой сервер
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
