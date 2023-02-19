715c951103f751a504f0035477ddb2acf20f469d
mak <pablo_57@mail.ru>
2023-02-19 20:16:30 +0300

    Incremet3 server uses chi-router

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"strconv"
@@ -56,8 +57,11 @@ type MemStorage struct {
 
 type Repository interface {
 	Init()
-	AddGauge(g GMetric)
-	AddCounter(c CMetric)
+	AddGauge(name string, val string) error
+	AddCounter(name string, val string) error
+	GetCounter(name string) (string, error)
+	GetGauge(name string) (string, error)
+	GetView() (map[string]string, error)
 }
 
 func (m *MemStorage) Init() {
@@ -65,26 +69,81 @@ func (m *MemStorage) Init() {
 	m.Counters = make(map[string]Counter)
 }
 
-func (m *MemStorage) AddGauge(g GMetric) {
+func (m *MemStorage) AddGauge(name string, val string) error {
 	if m == nil {
-		return
+		return errors.New("m *MemStorage is nil. Init it first")
 	}
 	if m.Gauges == nil {
-		return
+		return errors.New("m.Gauges map[string]gauge is nil")
+	}
+	g := GMetric{}
+	err := g.TryParse(name, val)
+	if err != nil {
+		log.Panic("gauge handler panic: value parse failed!")
+		return err
 	}
 	m.Gauges[g.Name] = g.Val
+	return nil
+
 }
 
-func (m *MemStorage) AddCounter(c CMetric) {
+func (m *MemStorage) AddCounter(name string, val string) error {
 	if m == nil {
-		return
+		return errors.New("m *MemStorage is nil. Init it first")
 	}
 	if m.Counters == nil {
-		return
+		return errors.New("m.Counters map[string]counter is nil")
 	}
+	c := CMetric{}
+	err := c.TryParse(name, val)
+	if err != nil {
+		log.Panic("counter handler panic: value parse failed!")
+		return err
+	}
+
 	if _, ok := m.Counters[c.Name]; !ok {
 		m.Counters[c.Name] = c.Val
-		return
+		return nil
 	}
 	m.Counters[c.Name] = m.Counters[c.Name] + c.Val
+	return nil
+}
+
+func (m *MemStorage) GetCounter(name string) (string, error) {
+	if m == nil {
+		return "", errors.New("m *MemStorage is nil. Init it first")
+	}
+	if m.Counters == nil {
+		return "", errors.New("m.Counters map[string]counter is nil")
+	}
+
+	if _, ok := m.Counters[name]; !ok {
+		return "404", fmt.Errorf("Counter with name %v is not found", name)
+	}
+	return fmt.Sprintf("%v", int64(m.Counters[name])), nil
+}
+
+func (m *MemStorage) GetGauge(name string) (string, error) {
+	if m == nil {
+		return "", errors.New("m *MemStorage is nil. Init it first")
+	}
+	if m.Gauges == nil {
+		return "", errors.New("m.Counters map[string]counter is nil")
+	}
+
+	if _, ok := m.Gauges[name]; !ok {
+		return "404", fmt.Errorf("Gauge with name %v is not found", name)
+	}
+	return fmt.Sprintf("%v", float64(m.Gauges[name])), nil
+}
+
+func (m *MemStorage) GetView() (map[string]string, error) {
+	view := make(map[string]string)
+	for key, val := range m.Counters {
+		view[key] = fmt.Sprintf("%v", val)
+	}
+	for key, val := range m.Gauges {
+		view[key] = fmt.Sprintf("%v", val)
+	}
+	return view, nil
 }
