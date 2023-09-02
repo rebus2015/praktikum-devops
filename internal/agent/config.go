6b408e23ed8f32b29cd88f8c25d0c0b79ca7b8a6
mak <pablo_57@mail.ru>
2023-09-02 04:44:50 +0300

    fix autotests copy agent from 14

--- internal/agent/config.go
+++ internal/agent/config.go
@@ -18,8 +18,8 @@ type Config struct {
 func GetConfig() (*Config, error) {
 	conf := Config{}
 	flag.StringVar(&conf.ServerAddress, "a", "127.0.0.1:8080", "Server address")
-	flag.DurationVar(&conf.ReportInterval, "r", time.Second*14, "Interval before push metrics to server")
-	flag.DurationVar(&conf.PollInterval, "p", time.Second*7, "Interval between metrics reads from runtime")
+	flag.DurationVar(&conf.ReportInterval, "r", time.Second*11, "Interval before push metrics to server")
+	flag.DurationVar(&conf.PollInterval, "p", time.Second*5, "Interval between metrics reads from runtime")
 	flag.StringVar(&conf.Key, "k", "", "Key to sign up data with SHA256 algorythm")
 	flag.IntVar(&conf.RateLimit, "l", 12, "Workers count")
 	flag.Parse()
