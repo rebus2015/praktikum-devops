677bfd81282687cce9333fbe031ea63666a59e28
mak <pablo_57@mail.ru>
2023-09-22 22:24:34 +0300

    changed metrics insert query

--- internal/storage/dbstorage/dbstorage.go
+++ internal/storage/dbstorage/dbstorage.go
@@ -12,16 +12,6 @@ const (
  UNIQUE (name,type)
         );
 
-CREATE OR REPLACE FUNCTION save(_name TEXT,_type VARCHAR(10),_value DOUBLE PRECISION,_delta bigint)
- RETURNS void AS '
-BEGIN
- INSERT INTO metrics (name,type,value,delta)
- VALUES (_name,_type,_value,_delta)
- ON CONFLICT(name,type) DO UPDATE
- SET value = EXCLUDED.value, delta = EXCLUDED.delta;
-END;
-' LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION get() 
     RETURNS TABLE ( name text, 
             type varchar(10), 
@@ -29,7 +19,8 @@ CREATE OR REPLACE FUNCTION get()
             delta bigint) AS $$
     SELECT name,type,value,delta FROM metrics
 $$ LANGUAGE SQL STABLE;`
-	SetMetricQuery  string = "SELECT save(@name,@type,@value,@delta)"
+	SetMetricQuery string = `INSERT INTO metrics (name,type,value,delta)
+        VALUES (@name,@type,@value,@delta)`
 	GetMetricsQuery string = "SELECT * FROM get()"
 )
 
